542ff290f4f1e54877d2f0711fc737319ed3acf6
Jeffrey Shen <jjshen@mit.edu>
2023-04-07 11:37:32 -0400

    create choropleth

--- src/components/Choropleth.svelte
+++ src/components/Choropleth.svelte
@@ -1,9 +1,36 @@
 <script>
     import "leaflet/dist/leaflet.css";
+    import departments from "../geojson/departments.json";
     import {onMount} from "svelte";
+    import * as d3 from "d3";
 
     let L;
     let map;
+
+    function f(d){
+        return d.COVER_2000 / d.TOTAL_SQUA;
+    }
+
+    function getDomain(data){
+        let min, max;
+        data.forEach((d) => {
+            let t = f(d.properties);
+            console.log(t)
+            if(isNaN(min)) min = t;
+            else min = Math.min(min, t);
+
+            if(isNaN(max)) max = t;
+            else max = Math.max(max, t);
+        });
+
+        return [min, max];
+    }
+
+    const colors = d3.scaleSequential(d3.interpolateGreens).domain(getDomain(departments.features));
+
+    function getColor(d){
+        return colors(f(d));
+    }
 
     onMount(async () => {
 
@@ -15,7 +42,10 @@
             attributionControl: false,
             dragging: false,
             doubleClickZoom: false,
-            boxZoom: false
+            boxZoom: false,
+            keyboard: false,
+            scrollWheelZoom: false,
+            touchZoom: false
         }).setView([15, -89], 6);
 
         var osm = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
@@ -23,6 +53,20 @@
             subdomains: 'abcd',
             maxZoom: 20
         }).addTo( map );
+
+        let departmentsLayer = L.geoJson(departments, {
+            style: function style(feature) {
+                return {
+                    fillColor: getColor(feature.properties),
+                    weight: 1,
+                    opacity: 1,
+                    color: 'white',
+                    fillOpacity: 0.9
+                };
+            }
+        }).addTo(map);
+
+        map.fitBounds(departmentsLayer.getBounds());
 
     });
 </script>
