a8a21f8f58b518f96e0d930421e9cb2c9ec96982
Jeffrey Shen <jjshen@mit.edu>
2023-04-27 14:02:45 -0400

    add country border

---
+++ src/components/CountryBorders.svelte
@@ -0,0 +1,25 @@
+<script>
+    import countries from "../geojson/countries.json";
+
+    export let L;
+    export let map;
+
+    let layer;
+
+    $: if(L && map){
+        map.createPane('borderPane');
+        map.getPane('borderPane').style.zIndex = 401;
+        layer = L.geoJson(countries, {
+            style: function style(feature) {
+                return {
+                    fillOpacity: 0,
+                    weight: 3,
+                    color: '#bbb'
+                };
+            },
+            pane: 'borderPane',
+            interactive: false
+        }).addTo(map);
+    }
+
+</script>
