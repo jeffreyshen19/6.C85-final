3b5ebfc4d06013293da1b2b35a8372ab164257b2
anneqliu <87202908+anneqliu@users.noreply.github.com>
2023-04-28 15:43:30 -0400

    Update HorizontalBarChart.svelte

---
+++ src/components/HorizontalBarChart.svelte
@@ -0,0 +1,122 @@
+<script>
+    import * as d3 from "d3";
+
+    export let title;
+	export let data;
+    export let highlighted = [];
+    $: highlightedIndexes = new Set(highlighted);
+
+    export let xAxisLabel = "";
+    export let xTicks;
+    export let formatXTick = (d) => d;
+    export let visible;
+
+	const padding = { top: 20, right: 15, bottom: 175, left: 25 };
+
+	let width = 500;
+	let height = 350;
+
+	const yScale = d3.scaleLinear()
+		.domain([0, data.length])
+		.range([height - padding.bottom, padding.top]);
+
+	const xScale = d3.scaleLinear()
+		.domain([0, Math.max.apply(null, xTicks)])
+		.range([padding.left, width - padding.right]);
+
+	const innerHeight = height - (padding.top + padding.bottom);
+	const barHeight = innerHeight / data.length;
+</script>
+
+<div class = "wrapper" style:opacity={visible ? 1 : 0}>
+    <h2>{title}</h2>
+    <div class="chart" bind:clientWidth={width} bind:clientHeight={height}>
+        <svg>
+            <!-- x axis -->
+            <g class="axis x-axis">
+                {#each xTicks as tick}
+                    <g class="tick tick-{tick}" transform="translate({padding.bottom}, {xScale(tick)})">
+                        <line y2="100%"></line>
+                        <text x="-4">{formatXTick(tick)}</text>
+                    </g>
+                {/each}
+
+                <g class = "x-axis-label" transform="translate(10, {width / 2}) rotate(-90)">
+                    <text y="{(width - padding.left - padding.right)/2}" style:text-anchor="middle">{xAxisLabel}</text>
+                </g>
+            </g>
+            <!-- y axis -->
+            <g class="axis x-axis">
+                {#each data as point, i}
+                    <g class="tick" transform="translate({padding.bottom + yScale(i) + barHeight / 2},{width - padding.left + 12})">
+                        <text transform="rotate(-50)" dy=".4em" dx="0.3em"  style:text-anchor="end">{point[0]}</text>
+                    </g>
+                {/each}
+            </g>
+    
+            <g class='bars'>
+                {#each data as point, i}
+                    <rect
+                        y="{yScale(i) + 2 + padding.bottom}"
+                        x="{xScale(point[1])}"
+                        height="{barHeight - 4}"
+                        width="{xScale(0) - yScale(point[1])}"
+                        stroke={highlightedIndexes.has(i) ? "#ede99d" : "none"} 
+                    ></rect>
+                {/each}
+            </g>
+        </svg>
+    </div>
+</div>
+
+<style>
+	h2 {
+		text-align: center;
+	}
+
+    .wrapper{
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 100%;
+		max-width: 500px;
+        transition: opacity 0.4s;
+    }
+
+	svg {
+		position: relative;
+		width: 100%;
+		height: 350px;
+	}
+
+	.tick, .y-axis-label     {
+		font-family: Helvetica, Arial;
+		font-size: .725em;
+		font-weight: 200;
+	}
+
+	.tick line {
+		stroke: #e2e2e2;
+		stroke-dasharray: 2;
+	}
+
+	.tick text {
+		fill: #ccc;
+		text-anchor: start;
+	}
+
+	.tick.tick-0 line {
+		stroke-dasharray: 0;
+	}
+
+	.x-axis .tick text {
+		text-anchor: middle;
+	}
+
+	.bars rect {
+		fill: #a11;
+        stroke-width: 3;
+		opacity: 0.65;
+	}
+</style>
