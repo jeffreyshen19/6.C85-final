43183783577fcd2a4d480292c175650e948a3db9
Jeffrey Shen <jjshen@mit.edu>
2023-05-05 16:01:34 -0400

    add section indicator

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -11,31 +11,33 @@
     style:background-color={background ? "white" : "rgba(0,0,0,0)"}
     style:opacity={progress < -0.09 || progress > 1.1 ? 0 : 1}
 >
-    <a href = "#intro">Intro</a>    
-    <a href = "#deforestation">Deforestation</a>
-    <a href = "#food-insecurity">Food Insecurity</a>
-    <a href = "#livelihood">Livelihood</a>
-    <a href = "#natural-disasters">Natural Disasters</a>
-    <a href = "#migration">Migration</a>
-    <a href = "#conclusion">Conclusion</a>
+    <a style:font-weight={progress <= 0 ? "bold" : 200} href = "#intro">Intro</a>    
+    <a style:font-weight={progress > 0 && index < 6 ? "bold" : 200} href = "#deforestation">Deforestation</a>
+    <a style:font-weight={index >= 6 && index < 13 ? "bold" : 200} href = "#food-insecurity">Food Insecurity</a>
+    <a style:font-weight={index >= 13 && index < 15 ? "bold" : 200} href = "#livelihood">Livelihood</a>
+    <a style:font-weight={index >= 15 && index < 19 ? "bold" : 200} href = "#natural-disasters">Natural Disasters</a>
+    <a style:font-weight={index >= 19 && progress < 1 ? "bold" : 200} href = "#migration">Migration</a>
+    <a style:font-weight={progress >= 1? "bold" : 200} href = "#conclusion">Conclusion</a>
 </nav>
 
 <style>
     nav{
-        width: 100vw;
         position: fixed;
         top: 0;
-        left: 0;
+        right: 0;
         z-index: 50000;
         padding: 15px 20px;
         transition: 0.2s all;
         box-sizing: border-box;
+        width: 100%;
+        text-align: left;
     }
 
     nav a{
         margin-right: 10px;
         text-decoration: none;
         color: black;
+        transition: 0.2s all;
     }
 
 </style>
